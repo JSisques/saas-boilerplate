086dd19867b7624cac4ccf67bfb4d9ecdfd06cec
JSisques <javierplazasisques@gmail.com>
2025-04-04 03:45:28 +0200

    feat(web, shadcn): add Radix UI components and update dependencies

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,12 +2,27 @@
 
 import Image from 'next/image';
 import { Button } from '@repo/shadcn/components/ui/button';
+
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@repo/shadcn/components/ui/accordion';
+import { Card, CardContent, CardHeader, CardTitle } from '@repo/shadcn/components/ui/card';
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
         <Button onClick={() => alert('Hello')}>Click me</Button>
+        <Accordion>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+          </AccordionItem>
+        </Accordion>
+        <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+          </CardHeader>
+          <CardContent>Card Content</CardContent>
+        </Card>
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{' '}
