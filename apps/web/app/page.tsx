6714e260de86dbddcc72eb2998121db970604ed4
JSisques <javierplazasisques@gmail.com>
2025-04-04 04:18:30 +0200

    refactor(web): clean up imports in page.tsx and adjust layout structure

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,9 +2,9 @@
 
 import Image from 'next/image';
 import { Button } from '@repo/shadcn/components/ui/button';
-
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@repo/shadcn/components/ui/accordion';
 import { Card, CardContent, CardHeader, CardTitle } from '@repo/shadcn/components/ui/card';
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
