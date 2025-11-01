20e116b5e1ef8a5fe43ffd59e09ffe85ef724520
JSisques <javierplazasisques@gmail.com>
2025-11-01 19:44:09 +0100

    feat(api): implement role-based access control for user queries
    - Added `Roles` decorator to the `findUsersByCriteria` query in `UserQueryResolver`, enforcing that only users with the `ADMIN` role can access this functionality.
    - Updated imports to include the new `UserRoleEnum` for better role management.

--- apps/api/src/user-context/users/transport/graphql/resolvers/user-queries.resolver.ts
+++ apps/api/src/user-context/users/transport/graphql/resolvers/user-queries.resolver.ts
@@ -1,3 +1,4 @@
+import { Roles } from '@/auth-context/auth/infrastructure/decorators/roles/roles.decorator';
 import { Criteria } from '@/shared/domain/entities/criteria';
 import { FindUsersByCriteriaQuery } from '@/user-context/users/application/queries/find-users-by-criteria/find-users-by-criteria.query';
 import { UserFindByCriteriaRequestDto } from '@/user-context/users/transport/graphql/dtos/requests/user-find-by-criteria.request.dto';
@@ -5,6 +6,7 @@ import { PaginatedUserResultDto } from '@/user-context/users/transport/graphql/d
 import { UserGraphQLMapper } from '@/user-context/users/transport/graphql/mappers/user.mapper';
 import { QueryBus } from '@nestjs/cqrs';
 import { Args, Query, Resolver } from '@nestjs/graphql';
+import { UserRoleEnum } from '@prisma/client';
 
 @Resolver()
 export class UserQueryResolver {
@@ -14,6 +16,7 @@ export class UserQueryResolver {
   ) {}
 
   @Query(() => PaginatedUserResultDto)
+  @Roles(UserRoleEnum.ADMIN)
   async findUsersByCriteria(
     @Args('input', { nullable: true }) input?: UserFindByCriteriaRequestDto,
   ): Promise<PaginatedUserResultDto> {
